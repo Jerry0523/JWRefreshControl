e43e4670039eba9257f49c9e791a10f7ffd99980
王杰 <wangjie51@meituan.com>
2019-07-05 18:44:58 +0800

    support SPM

---
+++ Package.swift
@@ -0,0 +1,12 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+    name: "JWRefreshControl",
+    products: [
+        .library(name: "JWRefreshControl", targets: ["JWRefreshControl"]),
+    ],
+    targets: [
+        .target(name: "JWRefreshControl", path: "JWRefreshControl")
+    ]
+)
